ccbc0b8c776ab56ffb72dcb83ca3500a8cb22dfd
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-10 17:54:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word (string)
   arr = string.split (" ")
   
-  arr = arr.collect do |word|
-    word.reverse
-  end
+  arr = arr.collect do {|word| word.reverse}
 
   arr.join (" ")
 end
